9f93494ead9a5aa9fa02aef05dae94e78aaea63d
Darya Korotkih <riyafonte@yahoo.com>
2017-11-19 13:42:17 +0300

    Ещё один вариант

--- src/D3_2.java
+++ src/D3_2.java
@@ -21,10 +21,21 @@ public class D3_2 {
         System.out.println(" \nEnter your favorite number");
         int num = in.nextInt();
 
+        System.out.println("==========================");
+
         System.out.println("A person named "+name+" lives in the city "+adrs+"\nThis person "+age+" years and he loves to engage in "+hobb);
+        System.out.println("==========================");
+        System.out.println("Имя:               "+name+
+                "\nГород:             "+adrs +
+                "\nВозраст:           "+age +
+                "\nХобби:             "+hobb);
+        System.out.println("==========================");
+
         System.out.println((char) 27 + "[31m\n\nThanks for the information provided,\n and now we will blow up your house " + (char)27 + "[0m");
 
-        System.out.println();
+
+
+
 
 
 
